8c463115a64235e084d74863bbd4820ea2031be6
Juriy Bura <juriy.bura@gmail.com>
2021-02-21 14:02:09 +0100

    Updated router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import { HashRouter as Router, Link, Route } from 'react-router-dom';
 
 function App() {
   return (
