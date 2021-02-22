634c41fc4a8eb5bf7b3e55571c5c052dca1dfd81
Juriy Bura <juriy.bura@gmail.com>
2021-02-22 10:40:17 +0100

    preparing for heroku deployment

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router basename="/my-other-page">
+    <Router>
       <div className="App">
         <ul className="menu">
           <li><Link to="/">Home</Link></li>
